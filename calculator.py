4c9a16beca7f0edb4bcb7f991d13da6809ce6366
konan-cyrille <dkonancyrille@gmail.com>
2021-02-12 16:50:09 +0100

    Ajout de fuction pour la soustraction et l'addition

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""Une calculatrice sous forme de bibliothèque, 
+contenant les fonction basique de math.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def substract(first_term, second_term):
+    return first_term - second_term
