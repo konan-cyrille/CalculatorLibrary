379eb302103ba1f4d6b8321080aa27d63da8a739
konan-cyrille <dkonancyrille@gmail.com>
2021-02-12 19:36:17 +0100

    erreur pep8

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,14 @@
     test unitaire de la librairie Calculatrice
 """
 
+
 import calculator
+
 
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
-    
+        assert 4 == calculator.add(2, 2)
+
     def tes_substraction(self):
         assert 2 == calculator.substract(4, 2)
